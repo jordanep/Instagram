abc8bddf23e7aee33c50e1163ba60de4868499ce
Jordan Peel <jordanep@fb.com>
2019-07-10 12:30:59 -0700

    pictures load now

--- app/src/main/java/com/example/instagram/PostAdapter.java
+++ app/src/main/java/com/example/instagram/PostAdapter.java
@@ -1,6 +1,8 @@
 package com.example.instagram;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +13,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.instagram.models.Post;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
@@ -38,7 +45,43 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
         Post post = posts.get(position);
         holder.tvUsername.setText(post.getUser().getUsername());
         holder.tvDescription.setText(post.getDescription());
+
+        ParseFile image = post.getImage();
+        String imageUrl = image.getUrl();
+        imageUrl = imageUrl.substring(4);
+        imageUrl = "https" + imageUrl;
+        Glide.with(context)
+                .load(imageUrl)
+                .placeholder(R.drawable.camera_shadow_fill)
+                .error(R.drawable.camera_shadow_fill)
+                .into(holder.ivPhoto);
+        //loadImages(image, holder.ivPhoto);
+        /*File imageFile = null;
+        try {
+            imageFile = image.getFile();
+            Bitmap imageBitmap = BitmapFactory.decodeFile(imageFile.getPath());
+            holder.ivPhoto.setImageBitmap(imageBitmap);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }*/
     }
+
+    /*private void loadImages(ParseFile thumbnail, final ImageView img) {
+
+        if (thumbnail != null) {
+            thumbnail.getDataInBackground(new GetDataCallback() {
+                @Override
+                public void done(byte[] data, ParseException e) {
+                    if (e == null) {
+                        Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                        img.setImageBitmap(bmp);
+                    } else {
+                    }
+                }
+            });
+        } else {
+        }
+    }*/
 
     @Override
     public int getItemCount() {
